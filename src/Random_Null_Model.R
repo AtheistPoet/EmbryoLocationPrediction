1058d05e626519b42d06e80cd885bd6c06da529d
AtheistPoet <tiit828@gmail.com>
2018-12-16 02:01:07 -0600

    Add files via upload

--- src/Random_Null_Model.R
+++ src/Random_Null_Model.R
@@ -38,7 +38,7 @@ Random.null.model <- function( X.RNAseq, distmap.obj, nboot, num.genes,
       dm.tmp <- mapCells(dm.tmp)
       dm.tmp@mcc.scores[is.nan(dm.tmp@mcc.scores)] <- 0
       # dir.results <- "/home/atheistpoet/Desktop/Work/Study/UIUC/Semester_1/CS598JP/Project/DREAM_single_cell/Data"
-      file <- paste(dir.MCC.rand, "/Random_MCC/MCC_Subchallenge_", j, "_boot_", i, 
+      file <- paste(dir.MCC.rand, "/MCC_Subchallenge_", j, "_boot_", i, 
                     ".RData", sep = "")
       MCC.rand <- dm.tmp@mcc.scores
       save(MCC.rand, file = file)
@@ -59,5 +59,48 @@ Random.null.model <- function( X.RNAseq, distmap.obj, nboot, num.genes,
       save(mismatch.vISH.rnd, file = file)
     }
   }
-  
+}
+
+
+
+
+##Random.MCC.dist: Calculate average distance of top 10 MCC locations 
+##random null model
+Random.MCC.dist <- function(X.RNAseq, dir.MCC.rand, dir.MCC.rand.dist, num.genes){
+  for( j in 1:length(num.genes) ){
+    mcc.dist.rand <- array(0, c(nboot, nrow(X.new)))
+    for( b in 1:nboot ){
+      cat("Num genes = ", num.genes[j], " boot loop = ", b, "\n")
+      dir.results <- "/home/atheistpoet/Desktop/Work/Study/UIUC/Semester_1/CS598JP/Project/DREAM_single_cell/Data"
+      file <- paste(dir.MCC.rand, "/MCC_Subchallenge_", j, "_boot_", b,
+                    ".RData", sep = "")
+      load(file)
+      id.rand <- apply(MCC.rand, 2, function(x){order(x, decreasing = T)[1:10]})
+      mcc.top10 <- apply(MCC.rand, 2, function(x){sort(x, decreasing = T)[1:10]})
+      id.maxmcc <- apply(dm@mcc.scores, 2, function(x){order(x, decreasing = T)[1]})
+      for( i in 1:nrow(X.new) ){
+        # mcc.corr.norm.rand[b, i] <- cor(dm@mcc.scores[, i],
+        #                                MCC.mat[, i])
+        # id.rand <- order(MCC.mat[, i], decreasing = T)[1:10]
+        # id.maxmcc <- order(dm@mcc.scores[, i], decreasing = T)[1]
+        if( nnzero(mcc.top10[, i]) != 0 ){
+          mcc.dist.rand[b, i] <- log10(1 + sqrt(sum((colSums(dm@geometry[id.rand[, i], ]*
+                                                               (cbind(mcc.top10[, i])%*%matrix(1, 1, 3)))/
+                                                       sum(mcc.top10[, i]) -
+                                                       (dm@geometry[id.maxmcc[i], ]))^2)))
+        }else{
+          mcc.dist.rand[b, i] <- log10(1 + sqrt(sum((colSums(dm@geometry[id.rand[, i], ]*
+                                                               (cbind(rep(1, length(mcc.top10[, i])))%*%matrix(1, 1, 3)))/
+                                                       length(mcc.top10[, i]) -
+                                                       (dm@geometry[id.maxmcc[i], ]))^2)))
+        }
+        
+      }
+      # mcc.corr.norm.rand[b, ] <- corr.maxmcc.rand
+      
+    }
+    file <- paste(dir.MCC.rand.dist, "/MCC_dist_rand_Subchallenge_", j, ".RData", 
+                  sep = "")
+    save(mcc.dist.rand, file = file)
+  }
 }
